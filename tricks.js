f91e5941071c91442e3bb002bb0e18a38e53b291
conanskyforce <conanskyforce@gmail.com>
2016-12-01 00:00:18 +0800

    js

---
+++ tricks.js
@@ -0,0 +1,13 @@
+var hiddenProperty = 'hidden' in document ? 'hidden' :    
+    'webkitHidden' in document ? 'webkitHidden' :    
+    'mozHidden' in document ? 'mozHidden' :    
+    null;
+var visibilityChangeEvent = hiddenProperty.replace(/hidden/i, 'visibilitychange');var tit=document.title;
+var onVisibilityChange = function(){
+    if (!document[hiddenProperty]) {    
+        document.title=tit;
+    }else{
+        document.title="hello!!!";
+    }
+}
+document.addEventListener(visibilityChangeEvent, onVisibilityChange);
