09784af53ef7bb7028a9a89c1c8b565837270b1b
conanskyforce <conanskyforce@gmail.com>
2016-12-05 09:58:07 +0800

    hook

---
+++ hook.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+import requests
+
+
+def get_key_info():
+	"""回调函数
+	"""
+	print response.headers['Content-Type']
+
+def main():
+
+	url="http://conanskyforce.tk"
+	requests.get(url)
+
+
+main()
