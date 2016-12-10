4bae9583a8a7ed55a44e4948c24e385a675d16d5
conanskyforce <conanskyforce@gmail.com>
2016-12-10 19:18:36 +0800

    python

---
+++ python/pythontrial.py
@@ -0,0 +1,13 @@
+class Person(object):
+    __count = 0
+    @classmethod
+    def how_many(cls):
+        return cls.__count
+    def __init__(self,name):
+        self.name = name
+        Person.__count = Person.__count + 1
+print (Person.how_many())
+p1 = Person('Bob')
+print (Person.how_many())
+print ('0')
+print ('1')
