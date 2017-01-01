4c576e9f6724a65cf679f8960fa46e7a01b6a545
conanskyforce <conanskyforce@gmail.com>
2017-01-01 11:25:20 +0800

    wxapp

---
+++ weixinapp/readMovie/data/posts-data.js
@@ -0,0 +1,95 @@
+var local_database = [
+    {
+        // alt+shift+f格式化代码
+        date: "Dev 31 2016",
+        title: "内容1",
+        imgSrc: "/images/content/1.jpg",
+        avatar: "/images/avatar/1.png",
+        img_condition: true,
+        content: "boxing serials",
+        reading: "1922",
+        collection: "200",
+        headImgSrc: "/images/post/1.jpg",
+        author: "柯凯",
+        dateTime: "24小时前",
+        detail: "又到了新的一年,这一年可谓悲欣交集,感慨良多呀",
+        postId: 0
+    }, {
+        date: "Dev 31 2016",
+        title: "内容2",
+        imgSrc: "/images/content/2.jpg",
+        avatar: "/images/avatar/1.png",
+        img_condition: true,
+        content: "boxing serials",
+        reading: "2012",
+        collection: "190",
+        headImgSrc: "/images/post/2.jpg",
+        author: "柯凯",
+        dateTime: "24小时前",
+        detail: "又到了新的一年,这一年可谓悲欣交集,感慨良多呀",
+        postId: 1
+    }
+    , {
+        date: "Dev 31 2016",
+        title: "内容3",
+        imgSrc: "/images/content/3.jpg",
+        avatar: "/images/avatar/1.png",
+        img_condition: true,
+        content: "boxing serials",
+        reading: "2012",
+        collection: "190",
+        headImgSrc: "/images/post/3.jpg",
+        author: "柯凯",
+        dateTime: "24小时前",
+        detail: "又到了新的一年,这一年可谓悲欣交集,感慨良多呀",
+        postId: 2
+    }
+    , {
+        date: "Dev 31 2016",
+        title: "内容4",
+        imgSrc: "/images/content/4.jpg",
+        avatar: "/images/avatar/1.png",
+        img_condition: true,
+        content: "boxing serials",
+        reading: "2012",
+        collection: "190",
+        headImgSrc: "/images/post/4.jpg",
+        author: "柯凯",
+        dateTime: "24小时前",
+        detail: "又到了新的一年,这一年可谓悲欣交集,感慨良多呀",
+        postId: 3
+    }
+    , {
+        date: "Dev 31 2016",
+        title: "内容5",
+        imgSrc: "/images/content/5.jpg",
+        avatar: "/images/avatar/1.png",
+        img_condition: true,
+        content: "boxing serials",
+        reading: "2012",
+        collection: "190",
+        headImgSrc: "/images/post/5.jpg",
+        author: "柯凯",
+        dateTime: "24小时前",
+        detail: "又到了新的一年,这一年可谓悲欣交集,感慨良多呀",
+        postId: 4
+    }
+    , {
+        date: "Dev 31 2016",
+        title: "内容6",
+        imgSrc: "/images/content/6.jpg",
+        avatar: "/images/avatar/1.png",
+        img_condition: true,
+        content: "boxing serials",
+        reading: "2012",
+        collection: "190",
+        headImgSrc: "/images/post/6.jpg",
+        author: "柯凯",
+        dateTime: "24小时前",
+        detail: "又到了新的一年,这一年可谓悲欣交集,感慨良多呀",
+        postId: 5
+    }
+]
+module.exports = {
+    postList: local_database
+}
