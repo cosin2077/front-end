b532601b93e09db5270d683a3861398e8d6b460f
conanskyforce <conanskyforce@gmail.com>
2017-07-24 12:02:49 +0800

    jsnotes

--- jsnotes.js
+++ jsnotes.js
@@ -11,3 +11,23 @@ alert(foo);
 
 // A：弹出的是1,因为函数申明会被提前,foo相当于局部变量
 
+// 下边代码的弹出顺序是什么?
+
+var x=3;
+
+var foo = {
+	x:2,
+	baz:{
+		x:1,
+		bar:function(){
+			return this.x;
+		}
+	}
+}
+var go = foo.baz.bar;
+alert(go())
+alert(go.bind(foo)())
+alert(foo.baz.bar())
+
+// A:弹出3,2,1
+
