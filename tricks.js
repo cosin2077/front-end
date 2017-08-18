b33dda827091838702b89bafec8fc8280700e5ca
conanskyforce <conanskyforce@163.com>
2017-08-18 16:24:02 +0800

    tk

--- tricks.js
+++ tricks.js
@@ -1,8 +1,10 @@
+
 var hiddenProperty = 'hidden' in document ? 'hidden' :    
     'webkitHidden' in document ? 'webkitHidden' :    
     'mozHidden' in document ? 'mozHidden' :    
     null;
-var visibilityChangeEvent = hiddenProperty.replace(/hidden/i, 'visibilitychange');var tit=document.title;
+var visibilityChangeEvent = hiddenProperty.replace(/hidden/i, 'visibilitychange');
+var tit=document.title;
 var onVisibilityChange = function(){
     if (!document[hiddenProperty]) {    
         document.title=tit;
