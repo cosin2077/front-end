2182cb9c8c770f70d41615e0c99cfc8af0764b55
conanskyforce <conanskyforce@163.com>
2017-11-13 16:33:22 +0800

    icodateA

---
+++ vue项目/static/js/icodateA.js
@@ -0,0 +1,417 @@
+var icodateA = [
+{
+	"tokenName":"Astronaut",
+	"tokenDate":"20 Sep &#x2013; 20 Oct",
+	"officialSite":"https://astronaut.capital",
+	"whitepaper":"https://astronaut.docsend.com/view/p4iazfu",
+	"tokenEnd":" 12 DAYS",
+	"icoMediaImg":"https://cdn.icodrops.com/wp-content/uploads/2017/09/Image-1.jpg",
+	"tokenType":"(CryptoFund)"
+},
+{
+	"tokenName":"COMSA",
+	"tokenDate":"2 Oct &#x2013; 6 Nov",
+	"officialSite":"https://comsa.io/",
+	"whitepaper":"https://comsa.io/ru/download/52847/",
+	"tokenEnd":" 29 DAYS",
+	"icoMediaImg":"https://cdn.icodrops.com/wp-content/uploads/2017/08/index1_3_ds90-900.jpg",
+	"tokenType":"(Blockchain Service)"
+},
+{
+	"tokenName":"LAToken",
+	"tokenDate":"22 Aug &#x2013; 10 Oct",
+	"officialSite":"https://sale.latoken.com",
+	"whitepaper":"https://sale.latoken.com/white-paper",
+	"tokenEnd":" 2 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/ILZDld1dWs8?feature=oembed",
+	"tokenType":"(Market)"
+},
+{
+	"tokenName":"HEROcoin",
+	"tokenDate":"14 Sep &#x2013; 14 Oct",
+	"officialSite":"https://www.herocoin.io",
+	"whitepaper":"https://s3-cdn.herocoin.io/HERO_Whitepaper.pdf",
+	"tokenEnd":" 6 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/mPzsplZh1xE?feature=oembed",
+	"tokenType":"(Bets)"
+},
+{
+	"tokenName":"Paragon",
+	"tokenDate":"15 Sep &#x2013; 15 Oct",
+	"officialSite":"https://paragoncoin.com/",
+	"whitepaper":"https://paragoncoin.com/whitepaper.pdf",
+	"tokenEnd":" 7 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/yRdeMrswT5o?feature=oembed",
+	"tokenType":"(Social)"
+},
+{
+	"tokenName":"Etherparty",
+	"tokenDate":"1 Oct &#x2013; 29 Oct",
+	"officialSite":"https://ico.etherparty.io",
+	"whitepaper":"https://etherparty.io/wp-content/uploads/2017/09/Etherparty_WhitePaper.pdf?75ba47&75ba47",
+	"tokenEnd":" 21 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/PL9KKcAZJwE?feature=oembed",
+	"tokenType":"(Smart Contract)"
+},
+{
+	"tokenName":"UnikoinGold",
+	"tokenDate":"22 Sep &#x2013; 22 Oct",
+	"officialSite":"https://unikoingold.com",
+	"whitepaper":"https://unikoingold.com/whitepaper",
+	"tokenEnd":" 14 DAYS",
+	"icoMediaImg":"https://cdn.icodrops.com/wp-content/uploads/2017/09/Unikoin-base.jpg",
+	"tokenType":"(Bets)"
+},
+{
+	"tokenName":"Doc.ai",
+	"tokenDate":"28 Sep &#x2013; 28 Oct",
+	"officialSite":"https://tokensale.doc.ai",
+	"whitepaper":"https://tokensale.doc.ai/pdf/whitepaper.pdf?_t=1505818134072",
+	"tokenEnd":" 20 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/b9z-HO7K7z0?feature=oembed",
+	"tokenType":"(Artificial Intelligence)"
+},
+{
+	"tokenName":"ATLANT",
+	"tokenDate":"7 Sep &#x2013; 31 Oct",
+	"officialSite":"https://atlant.io",
+	"whitepaper":"https://atlant.io/assets/documents/en/Atlant_WP_publish.pdf",
+	"tokenEnd":" 23 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/ACTZ9ZsVZfc?feature=oembed",
+	"tokenType":"(Real Estate)"
+},
+{
+	"tokenName":"Ignis",
+	"tokenDate":"5 Aug &#x2013; 4 Nov",
+	"officialSite":"https://www.jelurida.com/ico",
+	"whitepaper":"https://www.jelurida.com/sites/default/files/JeluridaWhitepaper.pdf",
+	"tokenEnd":" 27 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/jFeeIHgpPUM?feature=oembed",
+	"tokenType":"(Blockchain)"
+},
+{
+	"tokenName":"Ties.Network",
+	"tokenDate":"21 Sep &#x2013; 21 Oct",
+	"officialSite":"https://ties.network",
+	"whitepaper":"https://cdn.ties.network/white-paper-eng.pdf",
+	"tokenEnd":" 13 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/PEX_V8tTwhY?feature=oembed",
+	"tokenType":"(Collaboration)"
+},
+{
+	"tokenName":"Enjin Coin",
+	"tokenDate":"3 Oct &#x2013; 31 Oct",
+	"officialSite":"https://enjincoin.io/",
+	"whitepaper":"https://enjincoin.io/enjincoin_whitepaper.pdf",
+	"tokenEnd":" 23 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/e9s4kQJXvHc?feature=oembed",
+	"tokenType":"(Gaming)"
+},
+{
+	"tokenName":"Aion Pre-sale",
+	"tokenDate":"3 Oct &#x2013; 10 Oct",
+	"officialSite":"https://aion.network/token-sale.html",
+	"whitepaper":"https://aion.network/downloads/aion.network_technical-introduction_en.pdf",
+	"tokenEnd":" 2 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/pFkPiL-dtDY?feature=oembed",
+	"tokenType":"(Blockchain)"
+},
+{
+	"tokenName":"Ambrosus",
+	"tokenDate":"22 Sep &#x2013; 22 Oct",
+	"officialSite":"https://tge.ambrosus.com",
+	"whitepaper":"http://ambrosus.com/wp-content/uploads/2017/07/Ambrosus-Vision-Paper-V.2.3.2-EN.pdf",
+	"tokenEnd":" 14 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/OkdCV6zw3lI?feature=oembed",
+	"tokenType":"(Blockchain Service)"
+},
+{
+	"tokenName":"Dragonchain",
+	"tokenDate":"2 Oct &#x2013; 2 Nov",
+	"officialSite":"https://dragonchain.com/",
+	"whitepaper":"https://dragonchain.com/assets/Dragonchain_Business_Summary.pdf",
+	"tokenEnd":" 25 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/UEogdXPaxeY?feature=oembed",
+	"tokenType":"(Blockchain Service)"
+},
+{
+	"tokenName":"Change Bank",
+	"tokenDate":"16 Sep &#x2013; 16 Oct",
+	"officialSite":"https://change-bank.com",
+	"whitepaper":"https://drive.google.com/file/d/0B4RKXcemFwwbNW5wVGtBMWM0c2s/view",
+	"tokenEnd":" 8 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/fHyzf-rQPdE?feature=oembed",
+	"tokenType":"(Banking)"
+},
+{
+	"tokenName":"PowerLedger",
+	"tokenDate":"8 Sep &#x2013; 6 Oct",
+	"officialSite":"https://powerledger.io",
+	"whitepaper":"https://powerledger.io/media/Power-Ledger-Whitepaper-v3.pdf",
+	"tokenEnd":" 6 October",
+	"icoMediaImg":"https://www.youtube.com/embed/9pow4WPFf6k?feature=oembed",
+	"tokenType":"(Blockchain Service)"
+},
+{
+	"tokenName":"Maecenas",
+	"tokenDate":"7 Sep &#x2013; 8 Oct",
+	"officialSite":"https://cofound.it/en/projects/maecenas/contribute/",
+	"whitepaper":"https://www.maecenas.co/Maecenas-WhitePaper.pdf",
+	"tokenEnd":" less 24 hours",
+	"icoMediaImg":"https://cdn.icodrops.com/wp-content/uploads/2017/09/amacenas11.jpg",
+	"tokenType":"(Marketplace)"
+},
+{
+	"tokenName":"GoldMint",
+	"tokenDate":"20 Sep &#x2013; 20 Oct",
+	"officialSite":"https://www.goldmint.io",
+	"whitepaper":"https://www.goldmint.io/white-paper",
+	"tokenEnd":" 12 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/xOm0bfGLADQ?feature=oembed",
+	"tokenType":"(Trading)"
+},
+{
+	"tokenName":"Wolk",
+	"tokenDate":"18 Sep &#x2013; 9 Oct",
+	"officialSite":"https://www.wolk.com/app/index",
+	"whitepaper":"https://www.wolk.com/whitepaper/WolkTokenGenerationEvent.pdf",
+	"tokenEnd":" 1 DAY",
+	"icoMediaImg":"https://www.youtube.com/embed/sWzRN5U-Y5o?feature=oembed",
+	"tokenType":"(Blockchain Service)"
+},
+{
+	"tokenName":"DomRaider",
+	"tokenDate":"12 Sep &#x2013; 11 Oct",
+	"officialSite":"https://www.domraider.io/en/",
+	"whitepaper":"https://s3-eu-west-1.amazonaws.com/domraider/domraider/DomRaider+ICO+Whitepaper+EN.pdf",
+	"tokenEnd":" 3 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/Ztn3-qhSpxo?feature=oembed",
+	"tokenType":"(Blockchain Service)"
+},
+{
+	"tokenName":"Starbase Pre-sale",
+	"tokenDate":"since 18 Sep",
+	"officialSite":"https://starbase.co/presale",
+	"whitepaper":"https://starbase.co/whitepaper",
+	"tokenEnd":"Not Set",
+	"icoMediaImg":"https://www.youtube.com/embed/OrOzZS4lHgA?feature=oembed",
+	"tokenType":"(Crowdfunding)"
+},
+{
+	"tokenName":"bitJob",
+	"tokenDate":"12 Sep &#x2013; 12 Oct",
+	"officialSite":"https://bitjob.io",
+	"whitepaper":"https://drive.google.com/file/d/0B6VVmGzOdFPBb3FCdUlsNGVWMWM/view?usp=sharing",
+	"tokenEnd":" 4 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/c-3ekS5Ahmc?feature=oembed",
+	"tokenType":"(Marketplace)"
+},
+{
+	"tokenName":"EventChain",
+	"tokenDate":"13 Sep &#x2013; 13 Oct",
+	"officialSite":"https://tokensale.eventchain.io",
+	"whitepaper":"https://eventchain.io/whitepaper.pdf",
+	"tokenEnd":" 5 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/HHKbQt0S2i0?feature=oembed",
+	"tokenType":"(Ticketing)"
+},
+{
+	"tokenName":"Anryze",
+	"tokenDate":"12 Sep &#x2013; 12 Oct",
+	"officialSite":"https://tokensale.anryze.com",
+	"whitepaper":"https://tokensale.anryze.com/assets/file/Anryze%20Distributed%20Network_EN.pdf",
+	"tokenEnd":" 4 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/f_61vIkYXLw?feature=oembed",
+	"tokenType":"(Blockchain Service)"
+},
+{
+	"tokenName":"DLSacademy",
+	"tokenDate":"15 Sep &#x2013; 15 Oct",
+	"officialSite":"http://dlsacademy.io",
+	"whitepaper":"http://dlsacademy.io/wp-content/uploads/2017/08/dlsacademy_white_paper.pdf",
+	"tokenEnd":" 7 DAYS",
+	"icoMediaImg":"https://player.vimeo.com/video/228714002",
+	"tokenType":"(Media)"
+},
+{
+	"tokenName":"DOVU",
+	"tokenDate":"3 Oct &#x2013; 17 Oct",
+	"officialSite":"https://dovu.io",
+	"whitepaper":"https://dovu.io/whitepaper.pdf",
+	"tokenEnd":" 9 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/_Rgas8gl1HQ?feature=oembed",
+	"tokenType":"(Currency)"
+},
+{
+	"tokenName":"Sand Coin",
+	"tokenDate":"18 Sep &#x2013; 18 Oct",
+	"officialSite":"https://en.sandcoin.io",
+	"whitepaper":"https://en.sandcoin.io/SNDWPENupdate.pdf?1",
+	"tokenEnd":" 10 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/KpBJZU2SXao?feature=oembed",
+	"tokenType":"(Real Business)"
+},
+{
+	"tokenName":"Gimli",
+	"tokenDate":"18 Sep &#x2013; 20 Oct",
+	"officialSite":"https://gimli.io",
+	"whitepaper":"https://gimli.io/wp-content/uploads/2017/07/Gimli-white-paper.pdf",
+	"tokenEnd":" 12 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/lY_Q4CbRhrQ?feature=oembed",
+	"tokenType":"(Bets)"
+},
+{
+	"tokenName":"COBINHOOD",
+	"tokenDate":"13 Sep &#x2013; 22 Oct",
+	"officialSite":"https://cobinhood.com/",
+	"whitepaper":"https://cobinhood.com/assets/whitepaper/whitepaper_en.pdf",
+	"tokenEnd":" 14 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/4Ro3kKnZIG8?feature=oembed",
+	"tokenType":"(Market)"
+},
+{
+	"tokenName":"IMMLA",
+	"tokenDate":"15 Sep &#x2013; 15 Oct",
+	"officialSite":"https://ico.immla.io",
+	"whitepaper":"https://immla.io/IMMLA_WP.pdf",
+	"tokenEnd":" 7 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/TUtwkDdiv-0?feature=oembed",
+	"tokenType":"(Blockchain Service)"
+},
+{
+	"tokenName":"Jibrel Network Pre-sale",
+	"tokenDate":"25 Sep &#x2013; 25 Oct",
+	"officialSite":"https://jibrel.network/",
+	"whitepaper":"https://jibrel.network/#whitepaper",
+	"tokenEnd":" 17 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/LBMyd7Ql8QU?feature=oembed",
+	"tokenType":"(Trading)"
+},
+{
+	"tokenName":"Snip",
+	"tokenDate":"29 Sep &#x2013; 29 Oct",
+	"officialSite":"https://www.snip.network",
+	"whitepaper":"https://www.snip.network/Snip_Whitepaper.pdf",
+	"tokenEnd":" 21 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/iy_6QAwFd9A?feature=oembed",
+	"tokenType":"(Media)"
+},
+{
+	"tokenName":"Soma",
+	"tokenDate":"26 Sep &#x2013; 26 Oct",
+	"officialSite":"https://soma.co",
+	"whitepaper":"https://soma.co/wp-content/uploads/2017/07/Whitepaper-2.pdf",
+	"tokenEnd":" 18 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/tOCpGfaaY3U?feature=oembed",
+	"tokenType":"(Marketplace)"
+},
+{
+	"tokenName":"Aeron",
+	"tokenDate":"19 Sep &#x2013; 23 Oct",
+	"officialSite":"https://aeron.aero",
+	"whitepaper":"https://aeron.aero/AeronWhitepaper.pdf",
+	"tokenEnd":" 15 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/ZOFE1KXLT-Y?feature=oembed",
+	"tokenType":"(Blockchain Service)"
+},
+{
+	"tokenName":"CryptoPay",
+	"tokenDate":"2 Oct &#x2013; 30 Oct",
+	"officialSite":"https://ico.cryptopay.me/#about",
+	"whitepaper":"https://ico.cryptopay.me/ico_cpay_wp.pdf",
+	"tokenEnd":" 22 DAYS",
+	"icoMediaImg":"https://cdn.icodrops.com/wp-content/uploads/2017/10/hero_main__background-d7633443bba9d906e3b1b9f8fa348620fd2d1be022cf7e66351d12c6667b5255.jpg",
+	"tokenType":"(Card)"
+},
+{
+	"tokenName":"Spheris",
+	"tokenDate":"19 Sep &#x2013; 19 Oct",
+	"officialSite":"https://spheris.io",
+	"whitepaper":"https://spheris.io/spheris_whitepaper.pdf",
+	"tokenEnd":" 11 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/ezJhMY6WLAw?feature=oembed",
+	"tokenType":"(Marketplace)"
+},
+{
+	"tokenName":"TokenStars",
+	"tokenDate":"10 Sep &#x2013; 31 Oct",
+	"officialSite":"https://tokenstars.com/en/ace/",
+	"whitepaper":"https://tokenstars.com/upload/files/ace_by_tokenstars_whitepaper.pdf",
+	"tokenEnd":" 23 DAYS",
+	"icoMediaImg":"https://player.vimeo.com/video/231101091",
+	"tokenType":"(Real Business)"
+},
+{
+	"tokenName":"Electroneum",
+	"tokenDate":"14 Sep &#x2013; 31 Oct",
+	"officialSite":"http://electroneum.com",
+	"whitepaper":"http://electroneum.com/overview-white-paper.pdf",
+	"tokenEnd":" 23 DAYS",
+	"icoMediaImg":"https://cdn.icodrops.com/wp-content/uploads/2017/09/Electroneum-Media.png",
+	"tokenType":"(Currency)"
+},
+{
+	"tokenName":"Dentacoin",
+	"tokenDate":"1 Oct &#x2013; 1 Nov",
+	"officialSite":"https://www.dentacoin.com",
+	"whitepaper":"https://www.dentacoin.com/white-paper/Whitepaper-en.pdf",
+	"tokenEnd":" 24 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/GntlhUrzqSw?feature=oembed",
+	"tokenType":"(Business)"
+},
+{
+	"tokenName":"Credit Protocol",
+	"tokenDate":"1 Oct &#x2013; 1 Nov",
+	"officialSite":"https://blockmason.io/projects/ico/",
+	"whitepaper":"https://blockmason.io/cp-whitepaper.pdf",
+	"tokenEnd":" 24 DAYS",
+	"icoMediaImg":"https://cdn.icodrops.com/wp-content/uploads/2017/09/CPT-Media.png",
+	"tokenType":"(Finance)"
+},
+{
+	"tokenName":"Crypto.tickets",
+	"tokenDate":"2 Nov &#x2013; 5 Nov",
+	"officialSite":"https://crypto.tickets/index.en.html",
+	"whitepaper":"https://crypto.tickets/crypto.tickets%20whitepaper%20ENG.pdf",
+	"tokenEnd":" 28 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/um5x_-TAzFc?feature=oembed",
+	"tokenType":"(Ticketing)"
+},
+{
+	"tokenName":"B2BX",
+	"tokenDate":"2 Oct &#x2013; 17 Nov",
+	"officialSite":"https://www.b2bx.pro",
+	"whitepaper":"https://www.b2bx.pro/docs/b2bx_white_paper-en.pdf",
+	"tokenEnd":" 40 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/YbaPQuHsNyE?feature=oembed",
+	"tokenType":"(Trading)"
+},
+{
+	"tokenName":"SRG",
+	"tokenDate":"since 4 Oct",
+	"officialSite":"https://srg.io",
+	"whitepaper":"https://srg.io/files/srg_white_paper_eng.pdf",
+	"tokenEnd":"Not Set",
+	"icoMediaImg":"https://www.youtube.com/embed/0LbvpfrpbTo?feature=oembed",
+	"tokenType":"(Gaming)"
+},
+{
+	"tokenName":"CarTaxi",
+	"tokenDate":"29 Sep &#x2013; 29 Oct",
+	"officialSite":"https://cartaxi.io",
+	"whitepaper":"https://cartaxi.io/#dl-whitepaper",
+	"tokenEnd":" 21 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/bQQjp_ujjXM?feature=oembed",
+	"tokenType":"(Real Business)"
+},
+{
+	"tokenName":"Presearch",
+	"tokenDate":"9 Sep &#x2013; 31 Oct",
+	"officialSite":"https://www.presearch.io",
+	"whitepaper":"https://www.presearch.io/uploads/WhitePaper.pdf",
+	"tokenEnd":" 23 DAYS",
+	"icoMediaImg":"https://www.youtube.com/embed/RTgnPd-eTuE?feature=oembed",
+	"tokenType":"(Network)"
+}];
+
+export default icodateA;
